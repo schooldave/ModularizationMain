f3b3f99f35fa603d07acc8e78d6304a31d3485c9
David <schooldave@sina.com>
2020-05-12 19:42:14 +0800

    InitMain

---
+++ ModularizationMain/Pods/Target Support Files/Pods-ModularizationMain-ModularizationMainUITests/Pods-ModularizationMain-ModularizationMainUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ModularizationMain_ModularizationMainUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ModularizationMain_ModularizationMainUITests
+@end
---
+++ ModularizationMain/Pods/Target Support Files/Pods-ModularizationMain/Pods-ModularizationMain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ModularizationMain : NSObject
+@end
+@implementation PodsDummy_Pods_ModularizationMain
+@end
